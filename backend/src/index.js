c9f9262d28aed7771f2a51be0c9ca7e41ae4364e
ehtashammd07 <mdehtasham165@gmail.com>
2025-04-26 23:18:41 +0530

    frontEnd Done

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,6 @@ import authRoutes from "./routes/auth.route.js"
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-
 import messageRoute from './routes/message.route.js'
 
 dotenv.config();
@@ -13,12 +12,16 @@ const app = express();
 
 const PORT = process.env.PORT;
 
-app.use(express.json());
-app.use(cookieParser());
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true,
 }));
+
+// app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(cookieParser());
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoute);
